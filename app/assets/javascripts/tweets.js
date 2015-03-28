2a01e7d2f87a6d8ff98d797aafa37f5e4fe616a8
kerwinkerwin <kerwin@enspiral.com>
2015-03-28 13:52:38 +1300

    maps displaying

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -6,11 +6,20 @@ $(document).ready(function(){
     var url = $(this).attr('action')
     url = url + "?"+data
     $.get(url).done(function(data){
+      loadScript();
       $.when(map.prototype.display_map()).then(map.prototype.hide_search());
       console.log(data)
     })
   })
 })
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.src = "http://maps.googleapis.com/maps/api/js?callback=initialize";
+  document.body.appendChild(script);
+}
+
+
 
 function map(){
 
