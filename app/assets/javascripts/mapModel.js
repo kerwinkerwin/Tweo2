6f707b1f19eeb850663d01acfa49655e67b26665
kerwinkerwin <kerwin@enspiral.com>
2015-06-01 14:22:36 +1200

    finally pins

--- app/assets/javascripts/mapModel.js
+++ app/assets/javascripts/mapModel.js
@@ -6,19 +6,7 @@ function mapModel(){
   this.map = new google.maps.Map(document.getElementById('map-canvas'),
       this.mapOptions);
     window.onload = this.loadScript;
-      // this.createMarker(this.map);
-      console.log(this.map);
 }
-
-mapModel.prototype.loadScript = function(){
-  var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
-        '&signed_in=true&callback=initialize';
-    document.body.appendChild(script);
-
-
-};
 
 mapModel.prototype.reSizeMap = function(){
   $('.gm-style').css('display','block');
@@ -27,17 +15,16 @@ mapModel.prototype.reSizeMap = function(){
 
 mapModel.prototype.createMarker = function(tweets){
   var googleMap = this.map;
-  console.log(this.map);
-  console.log(googleMap)
   var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(tweets[1], tweets[0]),
+    position: new google.maps.LatLng(tweets[0], tweets[1]),
     title: "hello",
   });
+  console.log(marker);
   marker.setMap(googleMap);
 };
 
 mapModel.prototype.addMarker = function(marker){
-  // marker.setMap(this.map);
+
 }
 
 mapModel.prototype.getTweets = function(formData){
@@ -50,7 +37,6 @@ mapModel.prototype.getTweets = function(formData){
     tweetController.renderMap();
     for(var i =0; i<data["tweets"].length; i++){
       if (data["tweets"][i].latitude != null) {
-        // pins.push([data["tweets"][i].latitude, data["tweets"][i].longitude]);
         createMarker([data["tweets"][i].latitude, data["tweets"][i].longitude]);
       };
     };
