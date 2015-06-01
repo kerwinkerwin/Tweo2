233ff8e5ac67f3a09126aca1fe07834eb7721836
kerwinkerwin <kerwin@enspiral.com>
2015-06-01 22:14:31 +1200

    Markers displaying text

--- app/assets/javascripts/mapModel.js
+++ app/assets/javascripts/mapModel.js
@@ -1,5 +1,4 @@
 function mapModel(){
-  setAllMap(null);
   this.mapOptions = {
     zoom: 1,
     center: new google.maps.LatLng(-41.282845, 174.765644)
@@ -7,6 +6,7 @@ function mapModel(){
   this.map = new google.maps.Map(document.getElementById('map-canvas'),
       this.mapOptions);
     window.onload = this.loadScript;
+
 }
 
 mapModel.prototype.reSizeMap = function(){
@@ -26,10 +26,19 @@ mapModel.prototype.createMarker = function(tweets){
 
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(tweets[0], tweets[1]),
-    title: "hello",
+    title: tweets[4],
     icon: pinImage,
   });
   marker.setMap(googleMap);
+  var infowindow = new google.maps.InfoWindow({
+      content: marker.title
+  });
+  google.maps.event.addListener(marker, 'mouseover', function() {
+    infowindow.open(googleMap,marker);
+  });
+  google.maps.event.addListener(marker, 'mouseout', function() {
+    infowindow.close(googleMap,marker);
+  });
 };
 
 mapModel.prototype.addMarker = function(marker){
@@ -46,7 +55,7 @@ mapModel.prototype.getTweets = function(formData){
     tweetController.renderMap();
     for(var i =0; i<data["tweets"].length; i++){
       if (data["tweets"][i][0] != null) {
-        createMarker([data["tweets"][i][0], data["tweets"][i][1], data["tweets"][i][2], data["tweets"][i][3]]);
+        createMarker([data["tweets"][i][0], data["tweets"][i][1], data["tweets"][i][2], data["tweets"][i][3],data["tweets"][i][4]]);
       };
     };
 
